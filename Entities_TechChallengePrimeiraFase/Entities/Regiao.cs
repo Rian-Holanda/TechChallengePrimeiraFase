664b8e5bccd13b0714df2f3542ef41508731aace
Douglas Castro <douglas.castro@rvc.law>
2024-05-16 21:17:06 -0300

    Configuração EF Core.

---
+++ Entities_TechChallengePrimeiraFase/Entities/Regiao.cs
@@ -0,0 +1,21 @@
+﻿using Entities_TechChallengePrimeiraFase.Entities;
+
+namespace Entities_TechChallengePrimeiraFase.Entities
+{
+    public class Regiao
+    {
+        //public int Id { get; set; }
+        //public required string Sigla { get; set; }    
+        //public required ICollection<ContatoPessoa> Contatos { get; set; }
+
+        public int Id { get; set; }
+        public string Sigla { get; set; }
+
+        // Propriedades de navegação
+        public ICollection<RegiaoCodigoArea> RegiaoCodigoAreas { get; set; }
+
+
+    }
+}
+
+
---
+++ Infrastructure_TechChallengePrimeiraFase/IApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Entities_TechChallengePrimeiraFase.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure_TechChallengePrimeiraFase
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Pessoa> Pessoas { get; set; }
+        DbSet<ContatoPessoa> ContatoPessoas { get; set; }
+        DbSet<Regiao> Regioes { get; set; }
+        DbSet<RegiaoCodigoArea> RegiaoCodigoAreas { get; set; }
+    }
+}
