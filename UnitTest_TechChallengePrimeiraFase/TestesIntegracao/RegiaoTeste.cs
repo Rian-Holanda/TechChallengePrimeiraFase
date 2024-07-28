6ee51d8636905c349dbc0b5ea30ca0edb9729bef
Rian-Holanda <55698491+Rian-Holanda@users.noreply.github.com>
2024-07-28 01:57:57 -0300

    teste de integração

---
+++ UnitTest_TechChallengePrimeiraFase/TestesIntegracao/RegiaoTeste.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest_TechChallengePrimeiraFase.TestesIntegracao
+{
+    public  class RegiaoTeste
+    {
+        [Fact]
+        //public async Task Get_ReturnDepartment_WhenDepartmentExist()
+        //{
+        //    Guid departmentId = Guid.Parse("f002392c-70e1-42dd-a3e9-467ee9c42284");
+
+        //    var httpClient = new HttpClient
+        //    {
+        //        BaseAddress = new Uri("http://localhost:7200") // the base address of our API projext
+        //    };
+
+        //    var response = await httpClient
+        //      .GetAsync($"/api/department/{departmentId}");
+
+        //    Assert.NotNull(response);
+
+        //    Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+        //    var department = await response.Content
+        //        .ReadFromJsonAsync<GetDepartmentResponse>();
+
+        //    Assert.Equal(departmentId, department.Id);
+        }
+    }
+}
