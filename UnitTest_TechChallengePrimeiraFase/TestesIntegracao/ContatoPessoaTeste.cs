61661cdc3c215cbc3fd47deb48656fa8e0e8bfaa
Rian-Holanda <55698491+Rian-Holanda@users.noreply.github.com>
2024-07-30 20:17:48 -0300

    teste deploy

---
+++ UnitTest_TechChallengePrimeiraFase/TestesIntegracao/ContatoPessoaTeste.cs
@@ -0,0 +1,34 @@
+﻿using API_TechChallengePrimeiraFase.Teste;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest_TechChallengePrimeiraFase.TestesIntegracao
+{
+    public class ContatoPessoaTeste : IClassFixture<WebApplicationFactory<IApiMarker>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public ContatoPessoaTeste(WebApplicationFactory<IApiMarker> appFactory)
+        {
+            _httpClient = appFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetPessoas()
+        {
+            var response = await _httpClient.GetAsync($"/Pessoa/GetPessoas");
+
+            var teste = response;
+
+            Assert.NotNull(response);
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+        }
+    }
+}
