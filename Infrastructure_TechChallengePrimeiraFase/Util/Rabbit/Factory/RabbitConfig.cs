9a4e317b7ae4d42f71cd14cc6612bfe6f8baf430
Rian-Holanda <55698491+Rian-Holanda@users.noreply.github.com>
2024-09-21 01:28:47 -0300

    configuração Rabbit

---
+++ Infrastructure_TechChallengePrimeiraFase/Util/Rabbit/Factory/RabbitConfig.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure_TechChallengePrimeiraFase.Util.Rabbit.Factory
+{
+    public class RabbitConfig
+    {
+        public ConnectionFactory Config()
+        { 
+            var factory = new ConnectionFactory() 
+            { 
+                HostName = "localhost",
+                UserName = "guest",
+                Port = 5672,
+                Password = "guest"
+                
+            
+            };
+
+            return factory;
+        }
+    }
+}
---
+++ UnitTest_TechChallengePrimeiraFase/TesteRabbit/TesteRabbit.cs
@@ -0,0 +1,28 @@
+﻿using Infrastructure_TechChallengePrimeiraFase.Util.Rabbit.Gateway;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest_TechChallengePrimeiraFase.TesteRabbit
+{
+    
+    public class TesteRabbit
+    {
+        [Fact]
+        public void TesteRabbitConfig() 
+        {
+            var mensagemPublish = "TESTE";
+            var mensagemConsume = "";
+
+            ProducerPessoa producerPessoa = new ProducerPessoa();
+
+            producerPessoa.TesteConexaoRabbitPublish(mensagemPublish);
+
+            mensagemConsume = producerPessoa.TesteConexaoRabbitConsume();
+
+            Assert.Equal(mensagemConsume, mensagemPublish);
+        }
+    }
+}
