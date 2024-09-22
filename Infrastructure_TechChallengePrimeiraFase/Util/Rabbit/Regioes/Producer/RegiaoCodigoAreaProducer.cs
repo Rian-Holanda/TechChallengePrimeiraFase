35d73c1bfc0c806e2691bdc8bb79116c6e37005a
Rian-Holanda <55698491+Rian-Holanda@users.noreply.github.com>
2024-09-22 20:42:06 -0300

    ajuste producer RegiaoCodigoArea

--- Infrastructure_TechChallengePrimeiraFase/Util/Rabbit/Regioes/Producer/RegiaoCodigoAreaProducer.cs
+++ Infrastructure_TechChallengePrimeiraFase/Util/Rabbit/Regioes/Producer/RegiaoCodigoAreaProducer.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure_TechChallengePrimeiraFase.Util.Rabbit.Regioes.Interface;
+﻿using Infrastructure_TechChallengePrimeiraFase.Util.Rabbit.Factory;
+using Infrastructure_TechChallengePrimeiraFase.Util.Rabbit.Regioes.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,47 @@ namespace Infrastructure_TechChallengePrimeiraFase.Util.Rabbit.Regioes.Producer
 {
     public class RegiaoCodigoAreaProducer : IRegiaoCodigoAreaProducer
     {
+
+        MensagemRabbit mensagemRabbit = new MensagemRabbit();
+        RabbitConfig rabbitConfig = new RabbitConfig();
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public bool InserirRegiaoCodigoArea(string json)
+        {
+            try
+            {
+                bool result = mensagemRabbit.PublicarMensagem(json, "InsertRegiaoCodigoArea");
+
+                return result;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool AlterarRegiaoCodigoArea(string json)
+        {
+            try
+            {
+                return mensagemRabbit.PublicarMensagem(json, "UpdateRegiaoCodigoArea");
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool ExcluirRegiaoCodigoArea(int id)
+        {
+            try
+            {
+                return mensagemRabbit.PublicarMensagem(id.ToString(), "ExcluirRegiaoCodigoArea");
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
